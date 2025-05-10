8488f75cf0c34dc474b72223b9a59657aa131d24
gvantsssiii <g.bagdava@gmail.com>
2025-05-10 18:22:16 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+  let userName = 'Giorgi'
+  let age = 16
+
+  return (
+   <div>
+    <h1>Hello world</h1>
+    <h2 className='blue'>My name is {userName}</h2>
+    <h2>I am {age} years old</h2>
+   </div>
+  )
+}
+
+export default App
+ 
